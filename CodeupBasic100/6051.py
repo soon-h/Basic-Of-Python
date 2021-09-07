4a63dd6c4c15c724c85050226c109be9ebfb33e6
soon-h <ksh8895420@naver.com>
2021-09-07 22:04:32 +0900

    Day4
    no.6051 ~ no.6060

---
+++ CodeupBasic100/6051.py
@@ -0,0 +1,5 @@
+a,b = input().split()
+if int(a) == int(b):
+    print(False)
+elif int(a) != int(b):
+    print(True)
---
+++ CodeupBasic100/6056.py
@@ -0,0 +1,8 @@
+num1,num2 = input().split()
+c1 = bool(int(num1))
+c2 = bool(int(num2))
+
+if c1 == c2:
+    print(False)
+else:
+    print(True)
